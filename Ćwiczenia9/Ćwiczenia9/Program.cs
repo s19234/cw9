b2d194fa86d468bbeb35a8d946012281f7ec2e74
Ryszard <s19234@pjwskt.edu.pl>
2020-05-04 11:51:57 +0200

    Ćwiczenia na LINQ

---
+++ Ćwiczenia9/Ćwiczenia9/Program.cs
@@ -0,0 +1,14 @@
+﻿using LinqConsoleApp;
+using System;
+
+namespace Ćwiczenia9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqSamples samples = new LinqSamples();
+            samples.Przyklad12();
+        }
+    }
+}
